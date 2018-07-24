11d1a7d839b8d05a75b51091bee13ae6ded004b2
Jonathon Gore <jackgore@hotmail.ca>
2018-07-23 22:31:10 -0400

    Styling team display

--- components/teams/TeamDisplay.js
+++ components/teams/TeamDisplay.js
@@ -1,8 +1,11 @@
-import Router from 'next/router';
-import Config from '../../config.js';
 import Button from '../misc/Button.js';
-import { Header } from '../general/display';
+import Config from '../../config.js';
+import Content from '../content/Content.js';
 import moment from 'moment';
+import Router from 'next/router';
+import Settings from './Settings';
+import { postData, half } from '../../util/util.js';
+import { Header, TwoPaneSplit } from '../general/display.js';
 import { getData } from '../../util/util.js';
 import '../../styles.scss';
 
@@ -10,8 +13,8 @@ const DATE_FORMAT = 'MMM Do YYYY';
 
 const Stats = (props) => (
   <div className='team-display-stats'>
-    <div className='team-display-members'>Members: {props.members}</div>
-    <div className='team-display-dob'>Created On: {moment(props.createdOn).format(DATE_FORMAT)}</div>
+    <span className='team-display-members'>Members: {props.members}</span>
+    <span className='team-display-dob'>Created On: {moment(props.createdOn).format(DATE_FORMAT)}</span>
   </div>
 );
 
@@ -45,6 +48,18 @@ class TeamDisplay extends React.Component {
       <div className='team-display'>
         <Header info={<Stats createdOn={this.state.createdOn} members={this.state.members}/>}
           title={this.props.teamName} noButton/>
+        {
+          this.state.showSettings ? (
+            <Settings onSubmit={this.addOrgMember}/>
+          ) : (
+            <div>
+              <div className='org-top-questions'>
+                <div className='org-questions-header'>Top Questions</div>
+                <Content org={this.props.orgName} />
+              </div>
+            </div>
+          )
+        }
       </div>
     );
   }
